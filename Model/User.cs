7b72775557f5af2ed7d1489872005d53fb354b53
idan_weiss <i.weiss.2006@gmail.com>
2023-10-30 16:15:36 +0200

    Add project files.

---
+++ Model/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User : BaseEntity
+    {
+        protected string firstname;
+        public string FirstName { get { return firstname; } set { firstname = value; } }
+
+        protected string lastname;
+        public string LastName { get { return lastname; } set { lastname = value; } }
+
+
+        protected string username;
+        public string UserName { get { return username; } set { username = value; } }
+
+        protected string password;
+        public string Password { get { return password; } set { password = value; } }
+
+        protected string email;
+        public string Email { get { return email; } set { email = value; } }
+
+        protected bool ismale;
+        public bool IsMale { get { return ismale; } set { ismale = value; } }
+
+
+        protected int accesslevel;
+        public int Accesslevel { get { return accesslevel; } set { accesslevel = value; } }
+        protected DateTime birthdate;
+        public DateTime Birthdate { get { return birthdate; } set { birthdate = value; } }
+
+    }
+
+    public class UserList : List<User>
+    {
+        public UserList() { }
+        public UserList(IEnumerable<User> list) : base(list) { }
+        public UserList(IEnumerable<BaseEntity> list) : base(list.Cast<User>().ToList()) { }
+    }
+}
---
+++ ViewModel/UserDB.cs
@@ -0,0 +1,46 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class UserDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new User();
+        }
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = entity as User;
+            user.ID = int.Parse(reader["Id"].ToString());
+            user.FirstName = reader["Firstname"].ToString();
+            user.LastName = reader["Lastname"].ToString();
+            user.UserName = reader["UserName"].ToString();
+            user.Password = reader["Password"].ToString();
+            user.Email = reader["Email"].ToString();
+            user.IsMale = bool.Parse(reader["IsMale"].ToString());
+            user.Accesslevel = int.Parse(reader["AccessLevel"].ToString());
+            user.Birthdate = DateTime.Parse(reader["Birthdate"].ToString());
+            return user;
+        }
+
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tblUsers";
+            UserList list = new UserList(ExecuteCommand());
+            return list;
+        }
+        public User SelectById(int id)
+        {
+            command.CommandText = "SELECT * FROM tblUsers WHERE Id=" + id;
+            UserList list = new UserList(ExecuteCommand());
+            if (list.Count == 0)
+                return null;
+            return list[0];
+        }
+    }
+}
